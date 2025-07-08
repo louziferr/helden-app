c480f3e2f09a252af858b83956ea93396e8e8bd4
luzieahrens <luzie.ahrens@gmail.com>
2025-07-08 12:44:00 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, redirect
+
+# App erstellen
+app = Flask(__name__)
+
+name = 'Naruto'
+# TODO Daten aus db/helden.json laden
+helden = ['Batman', 'Pikachu', 'Spongebob']
+
+# Route definieren
+@app.route('/')
+def index():
+    return render_template('index.html', name=name, helden=helden)
+
+# zweite Route definieren
+@app.route('/neu', methods=('GET', 'POST'))
+def neu():
+    if request.method == 'POST':
+        neuer_name = request.form.get('neuer_name')
+        helden.append(neuer_name)
+        # TODO Neuen Namen in db/helden.json speichern
+        return redirect('/')
+    else:
+        # Die Methode ist GET
+        return render_template('neuer_name.html')
+
+# App starten
+app.run(debug=True)
